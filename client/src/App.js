2ed0f808f337b35e2df40b482e412b9b70708235
Bui Minh Dung <85019719+junzmint@users.noreply.github.com>
2023-06-21 22:21:30 +0700

    func(): login form and logout button demo + server PORT change 3001

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+
+function LoginForm() {
+  const [phone, setPhone] = useState('');
+  const [password, setPassword] = useState('');
+  const [role, setRole] = useState('ADMIN');
+  const [rememberMe, setRememberMe] = useState(false);
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    const response = await fetch('http://localhost:5000/auth/login', {
+      method: 'POST',
+      body: JSON.stringify({ phone, password, role }),
+      headers: { 'Content-Type': 'application/json' }
+    });
+    const data = await response.json();
+    if (response.ok) {
+      if (rememberMe) {
+        localStorage.setItem('jwt', data.data.access_token);
+      } else {
+        sessionStorage.setItem('jwt', data.data.access_token);
+      }
+      // Redirect to the home page or the page the user was trying to access
+      window.location.href = '/';
+    } else {
+      // Display an error message
+      console.error(data.message);
+    }
+  };
+
+  return (
+    <form onSubmit={handleLogin}>
+      <label>
+        phone:
+        <input type="text" value={phone} onChange={(event) => setPhone(event.target.value)} />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input type="password" value={password} onChange={(event) => setPassword(event.target.value)} />
+      </label>
+      <br />
+      <label>
+        Remember me:
+        <input type="checkbox" checked={rememberMe} onChange={(event) => setRememberMe(event.target.checked)} />
+      </label>
+      <br />
+      <button type="submit">Login</button>
+    </form>
+  );
+}
+
+function LogoutButton() {
+  const handleLogout = () => {
+    localStorage.removeItem('jwt');
+    sessionStorage.removeItem('jwt');
+    // Redirect to the login page
+    window.location.href = '/login';
+  };
+
+  return (
+    <div>
+    <h1>Hello</h1>
+    <button onClick={handleLogout}>Logout</button>
+    </div>
+  );
+}
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const checkAuthentication = async () => {
+    const jwt = localStorage.getItem('jwt') || sessionStorage.getItem('jwt');
+    if (jwt) {
+      const response = await fetch('/api/authenticate', {
+        method: 'GET',
+        headers: { 'Authorization': `Bearer ${jwt}` }
+      });
+      if (response.ok) {
+        setIsAuthenticated(true);
+      } else {
+        localStorage.removeItem('jwt');
+        sessionStorage.removeItem('jwt');
+      }
+    }
+  };
+
+  useEffect(() => {
+    checkAuthentication();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {isAuthenticated ? <LogoutButton /> : <LoginForm />}
     </div>
   );
 }
