439bff52e2d329c3cab063d0192820fcc3dd0e0f
Bui Minh Dung <85019719+junzmint@users.noreply.github.com>
2023-06-24 13:19:58 +0700

    fea: phase 1 format structure && auth/protected to auth token

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,19 +2,31 @@ const express = require('express');
 
 const { authToken } = require('../middlewares/is-auth');
 const authController = require('../controllers/auth');
-const validator = require("../middlewares/validator")
+const validator = require('../middlewares/validator');
 
 const { tryCatch } = require('../middlewares/errorHandler');
 
 const router = express.Router();
 
+router.get('/protected', authToken, function (req, res) {
+  res.json({ message: 'You are authorized to access this resource.' });
+});
+
 router.post('/login', validator.login, tryCatch(authController.login));
 
-router.post('/genToken',  tryCatch(authController.generateToken));
+router.post('/genToken', tryCatch(authController.generateToken));
 
-router.post('/genResetToken', validator.genResetToken, tryCatch(authController.genResetToken));
+router.post(
+  '/genResetToken',
+  validator.genResetToken,
+  tryCatch(authController.genResetToken)
+);
 
-router.patch('/resetPassword', validator.resetPassword, tryCatch(authController.resetPassword));
+router.patch(
+  '/resetPassword',
+  validator.resetPassword,
+  tryCatch(authController.resetPassword)
+);
 
 router.post('/logout', authToken, tryCatch(authController.logout));
 
