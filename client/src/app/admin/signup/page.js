9f429d20e50986c73889a706abcc967888824406
vandanghuy9 <vandanghuy9@gmail.com>
2023-06-30 11:46:57 +0700

    feat: create dashboard for each role

---
+++ client/src/app/admin/signup/page.js
@@ -0,0 +1,12 @@
+'use client'
+import React from "react"
+const SignUp = ()=>{
+    return 
+    (<>
+    <form>
+        
+        <button type="submit">Sign up</button>
+    </form>
+    </>)
+}
+export default SignUp;
