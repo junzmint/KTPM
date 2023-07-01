d9b216ed659f0617952635473f596a5718a12fea
Bui Minh Dung <85019719+junzmint@users.noreply.github.com>
2023-07-01 15:41:43 +0700

    fea: css login and accountant

---
+++ client/src/app/accountant/fee/createFee.js
@@ -0,0 +1,17 @@
+'use client';
+
+import React from 'react';
+
+
+const createFeeForm = () =>{
+
+
+  return(
+    <div class="w-full py-2 text-xl md:text-3xl font-bold text-center">
+          Form Name
+        </div>
+  );
+} ;
+
+
+export default createFeeForm;
--- client/src/app/accountant/fee/page.js
+++ client/src/app/accountant/fee/page.js
@@ -1,32 +1,36 @@
 'use client';
+import BlueButton from '@/components/button/blue-button';
 import Navbar from '@/components/navbar';
 import React, { useState, useEffect } from 'react';
-
-
-
+import Popup from 'reactjs-popup';
 const Fee = () => {
   const [fee, setFeeList] = useState([]);
   const [donation, setDonationList] = useState([]);
-  const HandleDelete = (id) => {
+
+  const handleDelete = async ({ _id }) => {
     const token =
       localStorage.getItem('access_token') ||
       sessionStorage.getItem('access_token');
     try {
-      const response = fetch(`http://localhost:4000/fee/delete/${id}`, {
-        method: "DELETE",
+      const response = await fetch(`http://localhost:4000/fee/delete/${_id}`, {
+        method: 'DELETE',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
           Authorization: `Bearer ${token}`,
         },
       });
-      if (response.ok){
-      
-      }else{
+      if (response.ok) {
+        setFeeList((prevList) => prevList.filter((item) => item._id !== _id));
+        setDonationList((prevList) =>
+          prevList.filter((item) => item._id !== _id)
+        );
+      } else {
         //Handle error
       }
     } catch (error) {
       console.error(error);
     }
+    console.log(_id);
   };
   useEffect(() => {
     (async () => {
@@ -75,70 +79,81 @@ const Fee = () => {
         <div className="flex flex-col w-[20%] bg-red-200">
           <Navbar data={jobMenu}></Navbar>
         </div>
-        <div className="flex flex-col w-[80%] bg-blue-200">
-          <div className="flex flex-row">
+        <div className="flex flex-col flex-auto w-4/5 h-screen overflow-y-scroll bg-slate-100">
+          <div className="w-full bg-white border-gray-200 rounded-lg shadow max-w-7xl">
+            <div className="flex flex-row ">
+              <div className="flex flex-col w-1/2">
+                <h1>List of fees</h1>
+              </div>
+              <div className="mr-0">
+                <Popup
+                  trigger={<BlueButton text="Create a fee"></BlueButton>}
+                >
+                  
+                </Popup>
+              </div>
+            </div>
             <div className="flex flex-col">
-              <h2>Fee</h2>
-            </div>
-          </div>
-          <div className="flex flex-row ">
-            <div className="flex flex-col w-1/2 bg-red-100">
-              <h1>List of fees</h1>
-            </div>
-            <div className="flex flex-col w-1/2 bg-blue-500">
-              <button>Create fee</button>
-            </div>
-          </div>
-
-          <div className="flex flex-col bg-yellow-200">
-            <table className="border border-black-green ">
-              <tr>
-                <th>Name</th>
-                <th>Cost</th>
-              </tr>
-              {fee.map((u, index) => (
-                <tr key={u._id}>
-                  <td>{u.name}</td>
-                  <td>{u.required}</td>
-                  <td>
-                    <button type="button" className="bg-green-600">Update</button>
-                    <button type="button" className="bg-red-600" onClick={HandleDelete(u._id)}>Delete</button>
-                  </td>
+              <table className="border border-black-green ">
+                <tr>
+                  <th>Name</th>
+                  <th>Cost</th>
                 </tr>
-              ))}
-            </table>
+                {fee.map((u, index) => (
+                  <tr key={u._id}>
+                    <td>{u.name}</td>
+                    <td>{u.required}</td>
+                    <td>
+                      <button type="button" className="bg-green-600">
+                        Update
+                      </button>
+                      <button
+                        type="button"
+                        className="bg-red-600"
+                        onClick={() => handleDelete(u)}
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </table>
+            </div>
           </div>
-          <div className="flex flex-row">
+          <div className="w-full mt-5 bg-white border-gray-200 rounded-lg shadow max-w-7xl">
+            <div className="flex flex-row ">
+              <div className="flex flex-col w-1/2 ">
+                <h1>List of donations</h1>
+              </div>
+              <BlueButton text="Create a donation"></BlueButton>
+            </div>
+
             <div className="flex flex-col">
-              <h2>Donation</h2>
-            </div>
-          </div>
-          <div className="flex flex-row ">
-            <div className="flex flex-col w-1/2 bg-red-100">
-              <h1>List of donations</h1>
-            </div>
-            <div className="flex flex-col w-1/2 bg-blue-500">
-              <button>Create donation</button>
-            </div>
-          </div>
-
-          <div className="flex flex-col bg-yellow-200">
-            <table className="border border-black-green ">
-              <tr>
-                <th>Name</th>
-                <th>Cost</th>
-              </tr>
-              {donation.map((u, index) => (
-                <tr key={u._id}>
-                  <td>{u.name}</td>
-                  <td>{u.required}</td>
-                  <td>
-                    <button type="button" className="bg-green-600">Update</button>
-                    <button type="button" className="bg-red-600">Delete</button>
-                  </td>
+              <table className="border border-black-green ">
+                <tr>
+                  <th>Name</th>
+                  <th>Cost</th>
                 </tr>
-              ))}
-            </table>
+                {donation.map((u, index) => (
+                  <tr key={u._id}>
+                    <td>{u.name}</td>
+                    <td>{u.required}</td>
+                    <td>
+                      <button type="button" className="bg-green-500">
+                        Update
+                      </button>
+                      <button
+                        type="button"
+                        className="bg-red-500"
+                        onClick={() => handleDelete(u)}
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </table>
+            </div>
           </div>
         </div>
       </div>
