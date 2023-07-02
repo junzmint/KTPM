9de66336824fe4cea38050e4cecac4aed0760758
rzngnam1402 <legiangnam1402@gmail.com>
2023-07-02 17:18:35 +0700

    feat: add new household member

--- client/src/components/button/button.js
+++ client/src/components/button/button.js
@@ -3,7 +3,6 @@ import React from 'react'
 function Button(props) {
     let { color } = props;
 
-    console.log(`text-white bg-${color}-700 hover:bg-${color}-800 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-${color}-600 dark:hover:bg-${color}-700 `);
     return (
         <button type="button" onClick={props.onClick} className={`text-white bg-${color}-700 hover:bg-${color}-800 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-${color}-600 dark:hover:bg-${color}-700 `}>{props.text}</button>
     )
