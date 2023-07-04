41231c28e6beab362e100a5f371b922b32604139
Do Chi Thanh <chithanhdo2002@gmail.com>
2023-07-04 11:42:24 +0700

    cart_id fix

--- client/src/app/leader/citizen/nhankhau/[id]/page.js
+++ client/src/app/leader/citizen/nhankhau/[id]/page.js
@@ -102,7 +102,7 @@ const Dashboard = ({ params }) => {
             </span>
           </div>
           <div className="ml-4">
-            <p>Căn cước công dân: {citizenCard.card_id} </p>
+            <p>Căn cước công dân: {citizenCard?.card_id} </p>
             <p>Ngày sinh: {citizenDoB} </p>
             <p>Hộ chiếu: {citizenDetail.passport_id}</p>
             <p>Quê quán: {citizenDetail.hometown}</p>
