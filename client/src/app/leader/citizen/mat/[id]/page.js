4984894451e3c7c1da67679ce949b3038da4c98c
Do Chi Thanh <chithanhdo2002@gmail.com>
2023-07-04 11:33:22 +0700

    Bug fix  + Text adjusted

--- client/src/app/leader/citizen/mat/[id]/page.js
+++ client/src/app/leader/citizen/mat/[id]/page.js
@@ -44,19 +44,19 @@ const Dashboard = ({ params }) => {
     data: [
       {
         id: 1,
-        name: "Dashboard",
+        name: "Trang chủ",
         path: "/leader",
         auth: userRoles,
       },
       {
         id: 2,
-        name: "Citizen",
+        name: "Công dân",
         path: "/leader/citizen",
         auth: userRoles,
       },
       {
         id: 3,
-        name: "Household",
+        name: "Hộ khẩu",
         path: "/leader/household",
         auth: userRoles,
       },
