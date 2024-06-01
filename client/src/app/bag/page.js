e5b7f333223a5a9a5b35c2ca01b9b2b262b3fc2e
vandanghuy120385 <van.dang.huy@sun-asterisk.com>
2024-06-01 10:40:09 +0700

    finish v1

---
+++ client/src/app/bag/page.js
@@ -0,0 +1,148 @@
+"use client";
+import React, { useEffect, useMemo, useState } from "react";
+import { useFetch } from "@/hooks/useFetch";
+import "./style.css";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+const getOptions = (title) => ({
+  indexAxis: "y",
+  elements: {
+    bar: {
+      borderWidth: 2,
+    },
+  },
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "right",
+    },
+    title: {
+      display: true,
+      text: title,
+    },
+  },
+});
+
+const Bag = () => {
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [selectedType, setSelectedType] = useState(0);
+  const [nounData, setNounData] = useState([]);
+  const [adjData, setAdjData] = useState([]);
+  const {
+    loading,
+    data: petDataNega,
+    error,
+  } = useFetch("?category=bag&type=negative");
+
+  const {
+    loading: loadingPos,
+    data: petDataPos,
+    error: errorPos,
+  } = useFetch("?category=bag&type=positive");
+  console.log(petDataNega);
+  useEffect(() => {
+    if (selectedCategory && selectedType) {
+      if (selectedType === "positive") {
+        console.log(`${selectedCategory}_noun`);
+        const noun = petDataPos[`${selectedCategory}_noun`];
+        const adj = petDataPos[`${selectedCategory}_adj`];
+        console.log(noun, adj);
+        setNounData(noun);
+        setAdjData(adj);
+      } else {
+        const noun = petDataNega[`${selectedCategory}_noun`];
+        const adj = petDataNega[`${selectedCategory}_adj`];
+        setNounData(noun);
+        setAdjData(adj);
+      }
+    }
+  }, [selectedCategory, selectedType]);
+  const nounLabel = nounData?.map((i) => i?.item);
+  const nounFrequency = {
+    labels: nounLabel,
+    datasets: [
+      {
+        label: "Vấn đề",
+        data: nounData?.map((i) => i.frequency),
+        borderColor:
+          selectedType === "positive"
+            ? "rgba(53, 162, 235, 0.5)"
+            : "rgb(255, 99, 132)",
+        backgroundColor:
+          selectedType === "positive"
+            ? "rgba(53, 162, 235, 0.5)"
+            : "rgb(255, 99, 132)",
+      },
+    ],
+  };
+  const adjLabel = adjData?.map((i) => i?.item);
+  const adjFrequency = {
+    labels: adjLabel,
+    datasets: [
+      {
+        label: "Cảm nhận người dùng",
+        data: adjData?.map((i) => i.frequency),
+        borderColor:
+          selectedType === "positive"
+            ? "rgba(53, 162, 235, 0.5)"
+            : "rgb(255, 99, 132)",
+        backgroundColor:
+          selectedType === "positive"
+            ? "rgba(53, 162, 235, 0.5)"
+            : "rgb(255, 99, 132)",
+      },
+    ],
+  };
+
+  if (loading || loadingPos) {
+    return <div>Loading....</div>;
+  }
+
+  return (
+    <div>
+      <div className="category-title">Lựa chọn danh mục</div>
+      <select
+        name="category"
+        id="category"
+        value={selectedCategory}
+        onChange={(e) => setSelectedCategory(e.target.value)}
+      >
+        <option value={""}>Chọn một danh mục</option>
+        <option value={"backpack"}>Balo</option>
+        <option value={"wallet"}>Ví</option>
+        <option value={"fanny-bag"}>Túi đeo chéo</option>
+        <option value={"laptop-bag"}>Túi laptop</option>
+      </select>
+      <select
+        name="category"
+        id="category"
+        value={selectedType}
+        onChange={(e) => setSelectedType(e.target.value)}
+      >
+        <option value={""}>Chọn một danh mục</option>
+        <option value={"positive"}>Comment tích cực</option>
+        <option value={"negative"}>Comment tiêu cực</option>
+      </select>
+      <Bar options={getOptions("Vấn đề quan tâm")} data={nounFrequency} />
+      <Bar options={getOptions("Nhận xét người dùng")} data={adjFrequency} />
+    </div>
+  );
+};
+
+export default Bag;
